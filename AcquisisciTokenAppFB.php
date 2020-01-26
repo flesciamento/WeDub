00c18ed0ea0792064a97e500d566cd7369139054
flesciamento <flesciamento@gmail.com>
2020-01-26 20:22:19 +0100

    Delete AcquisisciTokenAppFB.php

--- AcquisisciTokenAppFB.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$DatiAPP = HTTPS_RichiestaGET("https://graph.facebook.com/oauth/access_token", "client_id=[INSERT CLIENT ID]&client_secret=[INSERT CLIENT SECRET]&grant_type=client_credentials");
-	
-$TokenAPP = $DatiAPP['access_token'];
-?>
