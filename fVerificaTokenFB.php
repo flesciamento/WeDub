cd9ba63123e969b330c3cf96207dabe2c6fb2a44
flesciamento <flesciamento@gmail.com>
2020-01-26 20:26:26 +0100

    Update fVerificaTokenFB.php

--- fVerificaTokenFB.php
+++ fVerificaTokenFB.php
@@ -7,7 +7,7 @@ function VerificaTokenFB($TokenDaEsaminare, $ID_Utente) {
 	
 	$Verifica = HTTPS_RichiestaGET("https://graph.facebook.com/debug_token", "input_token=$TokenDaEsaminare&access_token=$TokenAPP");
 	
-	return !(isset($Verifica['error']) || isset($Verifica['data']['error']) || ($Verifica['data']['app_id'] != "806415089524313") || ($Verifica['data']['user_id'] != $ID_Utente));
+	return !(isset($Verifica['error']) || isset($Verifica['data']['error']) || ($Verifica['data']['app_id'] != "[APP ID]") || ($Verifica['data']['user_id'] != $ID_Utente));
 }
 ?>
 
