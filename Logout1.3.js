4dee7df36dcce3027a4d4d7cf2eb6649f4fea5be
flesciamento <flesciamento@gmail.com>
2022-07-07 15:02:44 +0200

    Delete Logout1.3.js

--- Logout1.3.js
+++
@@ -1,3 +0,0 @@
-function Logout(Accidentale) {
-	window.location.href = "https://" + window.location.hostname + "/WEDUB/Logout.php" + (Accidentale? "?Pag=" + encodeURIComponent(window.location.href) : "");
-}
