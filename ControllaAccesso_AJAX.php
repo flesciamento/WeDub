293c709dcc6f11de5be0fa82efc7343a3a0333a7
flesciamento <flesciamento@gmail.com>
2022-07-07 15:00:09 +0200

    Delete ControllaAccesso_AJAX.php

--- ControllaAccesso_AJAX.php
+++
@@ -1,3 +0,0 @@
-<?php Session_start();
-    if (!$_SESSION['ID']) {Torna("Non sei connesso al sito, <a href='https://" . $_SERVER['SERVER_NAME'] . "/WEDUB/Login.php'>rifai il login!</a>", true);}
-?>
