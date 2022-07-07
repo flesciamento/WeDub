39275926efeb4abbfbe2e4f77ec49cff8c027a11
flesciamento <flesciamento@gmail.com>
2022-07-07 14:59:37 +0200

    Delete CaricaImmagineProfiloDefault.php

--- CaricaImmagineProfiloDefault.php
+++
@@ -1,7 +0,0 @@
-<?php
-$_FILES["Foto"]["name"] = $_POST['Nome'];
-file_put_contents($NomeFileProvvisorio = "FotoProfilo/Provvisorio" . time(), file_get_contents($_POST['Percorso']));
-$_FILES["Foto"]["tmp_name"] = $NomeFileProvvisorio;
-
-require("CaricaFotoProfilo.php");
-?>
