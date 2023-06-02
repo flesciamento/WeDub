6ec2cfb02b06497386eac96cb6a38bbcf7c7c334
flesciamento <flesciamento@gmail.com>
2023-06-02 20:48:34 +0200

    DatiCopione

---
+++ TrascrizioneVideo_FunzioniComuni1.0.js
@@ -0,0 +1,13 @@
+const DatiCopione = {};
+
+function AggiornaTestoGuida() {
+    clearTimeout(AggiornaTestoGuida.tmr);
+    AggiornaTestoGuida.tmr = setTimeout(() => {
+        const MinutaggioAttuale = VideoGuidaMinutaggioCorrente();
+        while(BT = DatiCopione.find(el => (((MinutaggioAttuale - 30) < el.minutaggio) && (el.minutaggio < (MinutaggioAttuale + 30))))) {
+            Testo += BT.testo;
+        }
+        TestoGuida.innerText = Testo;
+    }, 1000);
+}
+AggiornaTestoGuida.tmr = false;
