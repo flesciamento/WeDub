8c5eacfad1a8e7ea3d6eeb7b0cbc57a78286d941
flesciamento <flesciamento@gmail.com>
2023-06-02 20:53:55 +0200

    Testo

---
+++ TrascrizioneVideo_FunzioniComuni1.2.js
@@ -0,0 +1,14 @@
+const DatiCopione = [];
+
+function AggiornaTestoGuida() {
+    clearTimeout(AggiornaTestoGuida.tmr);
+    AggiornaTestoGuida.tmr = setTimeout(() => {
+        const MinutaggioAttuale = VideoGuidaMinutaggioCorrente();
+        var Testo = "";
+        while(BT = DatiCopione.find(el => (((MinutaggioAttuale - 30) < el.minutaggio) && (el.minutaggio < (MinutaggioAttuale + 30))))) {
+            Testo += BT.testo;
+        }
+        TestoGuida.innerText = Testo;
+    }, 1000);
+}
+AggiornaTestoGuida.tmr = false;
