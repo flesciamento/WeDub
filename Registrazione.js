2b49215ce2c36baee4d1d12e05ad9766b469b583
flesciamento <flesciamento@gmail.com>
2025-03-11 15:36:03 +0100

    minori

--- Registrazione.js
+++ Registrazione.js
@@ -3774,6 +3774,7 @@ function CreaFinestraOpzioniClip(RiferimentoRegistrazione) {
         /******************************/
 
         /*** Posizione ed effetti della clip ***/
+        const divContenitoreMinEff_id = ID_Opzioni + 'divContenitoreMinEff', tabellaminutaggio_id = ID_Opzioni + 'TabellaMinutaggio';
         const divContenitoreMinEff = CreaElemento('div', ID_Opzioni + 'divContenitoreMinEff', divcontenitorebody.id);
                                         /* Minutaggio */
             const tabellaminutaggio = CreaElemento('table', ID_Opzioni + 'TabellaMinutaggio', divContenitoreMinEff.id);
@@ -3830,17 +3831,17 @@ function CreaFinestraOpzioniClip(RiferimentoRegistrazione) {
         const stiliMenu = {textAlign: "left", width: "100%"}, NomeFile = `${NomeProgetto} ${NomeDoppiaggio} ${DatiDoppiatori[datiAudio.ID_Utente].nome} ${strBattuta} ${strA} ${MinutaggioAttuale.Minuti.duecifre()} min ${MinutaggioAttuale.Secondi.toFixed(3)} sec${datiAudio.Registrazione.slice(datiAudio.Registrazione.lastIndexOf("."))}`.replace(/[^a-zA-Z0-9\.]/g, "_");
         const divOpzioniVarie_id = ID_Opzioni + 'divOpzioniRigaVarie', btnMenuAscolta_id = ID_Opzioni + 'grpPulAscolta', btnMenuScarica_id = ID_Opzioni + 'grpMenuScarica', pulAscolta_id = ID_Opzioni + 'PulAscolta', ulPulAscolta_id = ID_Opzioni + 'ulMenuPulAscolta', ulPulScarica_id = ID_Opzioni + 'ulMenuPulScarica', liAscoltaSoloTaglio_id = ID_Opzioni + "AscoltaSoloTaglio";
         CreaNuoviElementi({
-            [ID_Opzioni + 'ContenitoreOpzioniBody']: [['div', {id: divOpzioniVarie_id}]],
-                [divOpzioniVarie_id]: [['div', {id: btnMenuAscolta_id, className: "btn-group"}, {marginRight: "5px"}], ['div', {id: btnMenuScarica_id, className: "btn-group"}]],
-                    [btnMenuAscolta_id]: [['a', {id: pulAscolta_id}, {}, {toggle: "dropdown", RiferimentoRegistrazione: RiferimentoRegistrazione}], ['ul', {id: ulPulAscolta_id, className: "dropdown-menu"}]],
-                        [ulPulAscolta_id]: [
-                            ['li', {id: liAscoltaSoloTaglio_id, innerHTML: "<span class='fa fa-exchange'></span> " + strSoloTaglio,   className: "btn btn-default", onclick: AscoltaInSolo_ParteTagliata}, stiliMenu],
-                            ['li', {innerHTML: "<span class='fa fa-toggle-right'></span> " + strDaInizioAFine,                                  className: "btn btn-default", onclick: AscoltaInSolo_Tutto}, stiliMenu],
-                            ['li', {innerHTML: "<span class='fa fa-refresh'></span> " + strCiclaClip,                                           className: "btn btn-info",    onclick: CiclaClip}, stiliMenu]
+            [ID_Opzioni + 'ContenitoreOpzioniBody']: [['div']],
+                [-1]: [['div', {id: btnMenuAscolta_id, className: "btn-group"}, {marginRight: "5px"}], ['div', {id: btnMenuScarica_id, className: "btn-group"}]],
+                    [btnMenuAscolta_id]: [['a', {id: pulAscolta_id}, {}, {toggle: "dropdown", RiferimentoRegistrazione: RiferimentoRegistrazione}], ['ul', {className: "dropdown-menu"}]],
+                        [-1]: [
+                            ['li', {id: liAscoltaSoloTaglio_id, innerHTML: "<span class='fa fa-exchange'></span> " + strSoloTaglio, className: "btn btn-default", onclick: AscoltaInSolo_ParteTagliata}, stiliMenu],
+                            ['li', {innerHTML: "<span class='fa fa-toggle-right'></span> " + strDaInizioAFine,                      className: "btn btn-default", onclick: AscoltaInSolo_Tutto}, stiliMenu],
+                            ['li', {innerHTML: "<span class='fa fa-refresh'></span> " + strCiclaClip,                               className: "btn btn-info",    onclick: CiclaClip}, stiliMenu]
                         ],
 
-                    [btnMenuScarica_id]: [['a', {innerHTML: strScaricaRegistrazione + " <span class='caret'></span>", className: "btn btn-default btn-sm fa fa-download", onclick: ApriMenuOpzioniScaricamento}, {}, {toggle: "dropdown", RiferimentoRegistrazione: RiferimentoRegistrazione}], ['ul', {id: ulPulScarica_id, className: "dropdown-menu"}]],
-                        [ulPulScarica_id]: [
+                    [btnMenuScarica_id]: [['a', {innerHTML: strScaricaRegistrazione + " <span class='caret'></span>", className: "btn btn-default btn-sm fa fa-download", onclick: ApriMenuOpzioniScaricamento}, {}, {toggle: "dropdown", RiferimentoRegistrazione: RiferimentoRegistrazione}], ['ul', {className: "dropdown-menu"}]],
+                        [-1]: [
                             ['li', {innerHTML: `<span class='fa fa-file-audio-o'></span> ${strScaricaClipOriginale}<a id='${ID_Opzioni}aScaricaOriginale' href="${datiAudio.Registrazione}" download="${NomeFile}" style='display: none;'></a>`, className: "btn btn-info", onclick: DownloadClipFinestraOpzioni}, stiliMenu],
                             ['li', {innerHTML: "<span class='fa fa-tasks'></span> " + strScaricaClipConversione, className: "btn btn-primary", onclick: DownloadClipConversione}, stiliMenu, {larghezza: "850px", altezza: "500px", RiferimentoRegistrazione: RiferimentoRegistrazione}]
                         ]
