282ba1ee4559c1ffd5c61d2a9339a54dedf1fdc8
flesciamento <flesciamento@gmail.com>
2025-10-12 22:32:25 +0200

    Pulsanti Modifica Copione

--- Registrazione.js
+++ Registrazione.js
@@ -1154,9 +1154,9 @@ VerificaModificaCopione_slow.tmr = false;
 function VerificaModificaCopione(divContenitore) {
     const id_pulSalva = divContenitore.id + "pulSalva", id_pulRipristina = divContenitore.id + "pulRipristina";
     if (!ContenitoreCopione.document.getElementById(id_pulRipristina) && (AcquisisciTestoCopione(divContenitore) != TrovaDatiCopioneID(divContenitore.dataset.numid).testo)) {
-        const NuoviPulsanti = [divContenitore];
-        if (SonoCreatoreProgetto) {NuoviPulsanti.push(['a', {id: id_pulSalva, className: "alert btn-warning", innerHTML: "<span class='fa fa-save'></span>", onclick: SalvaCopioneModificato}, {margin: "10px 5px", padding: "0 5px", lineHeight: 2}, {idcontenitore: divContenitore.id}]);}
-        NuoviPulsanti.push(['a', {id: id_pulRipristina, className: "alert btn-warning", innerHTML: "<span class='fa fa-undo'></span>", onclick: RipristinaCopioneModificato}, {margin: "10px 5px", padding: "0 5px", lineHeight: 2}, {idcontenitore: divContenitore.id}]);
+        const NuoviPulsanti = [divContenitore], stilipulsanti = {margin: "10px 5px", padding: "0 5px", lineHeight: 2};
+        if (SonoCreatoreProgetto) {NuoviPulsanti.push(['a', {id: id_pulSalva, className: "alert btn-warning", innerHTML: "<span class='fa fa-save'></span>", onclick: SalvaCopioneModificato}, stilipulsanti, {idcontenitore: divContenitore.id}]);}
+        NuoviPulsanti.push(['a', {id: id_pulRipristina, className: "alert btn-warning", innerHTML: "<span class='fa fa-undo'></span>", onclick: RipristinaCopioneModificato}, stilipulsanti, {idcontenitore: divContenitore.id}]);
         CreaNuoviElementi(NuoviPulsanti);
     }
 }
